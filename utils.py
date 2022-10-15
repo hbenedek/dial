9ea8972f9f812873ba2215e79fd4121ed6e7cf1b
Benedek Harsanyi <hben.0204@gmail.com>
2022-10-15 15:57:19 +0200

    add scraper

--- utils.py
+++ utils.py
@@ -1,5 +1,32 @@
-import logging
 import numpy as np
+import logging
+import torch
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def tabu_scraper(path = "data/tabu/"):
+    """scrapes the benchmark dataset from Cordeau, Laporte 2003"""
+    for i in range(1,21):
+        instance = str(i).zfill(2)
+        url = f"http://neumann.hec.ca/chairedistributique/data/darp/tabu/pr{instance}"
+        filename = f"pr{instance}.txt"
+        urllib.request.urlretrieve(url, path+filename)
+
+def branch_cut_scraper():
+    """scrapes the benchmark dataset from Cordeau 2006"""
+    root = "http://neumann.hec.ca/chairedistributique/data/darp/branch-and-cut/"
+    page = requests.get(root).text
+    soup = BeautifulSoup(page, 'html.parser')
+    urls = [root + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith('')][1:]
+    
+    for url in urls:
+        filename = re.sub("(.*)//","", url)
+        url = root + filename
+        path = "data/cordeau/"
+        print(f"scraping {url}")
+        urllib.request.urlretrieve(url, path+filename+".txt")
 
 def float_equality(f1: float, f2: float, eps: float=0.001) -> bool:
     return abs(f1 - f2) < eps
@@ -21,3 +48,20 @@ def init_logger(log_path: str=".", file_name: str="log") -> logging.Logger:
     logger.setLevel(logging.DEBUG)
     return logger
 
+def coord2int(coord: float) -> int:
+    """given a float transforms it to integer representation"""
+    number_precision = 3
+    new_coord = int(round(coord, number_precision)*(10**number_precision))
+    return new_coord
+
+def get_device() -> str:
+    if torch.cuda.is_available(): 
+        device = 'cuda:0'
+    else:
+        device = 'cpu'
+    return device
+
+
+if __name__ == "__main__":
+    tabu_scraper()
+    branch_cut_scraper()
